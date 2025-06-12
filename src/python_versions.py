ee4aaa5c5686fb9e094f9dec35d1de30a5811fc0
Derek Harter <derek@harter.pro>
2025-06-12 01:44:32 +0000

    Remove tensorflow from python_versions.py
    Also had an import in the test of versions for
    GitHub tasks, remove it.

--- src/python_versions.py
+++ src/python_versions.py
@@ -7,7 +7,7 @@ import sklearn as sk
 import matplotlib as mp
 import scipy as sp
 import pandas as pd
-import tensorflow as tf
+#import tensorflow as tf
 
 print('Python version    : ', sys.version)
 print('numpy version     : ', np.__version__)
@@ -15,9 +15,9 @@ print('scikit-learn  vers: ', sk.__version__)
 print('matplotlib version: ', mp.__version__)
 print('scipy version     : ', sp.__version__)
 print('pandas version    : ', pd.__version__)
-print('tensorflow version: ', tf.__version__)
+#print('tensorflow version: ', tf.__version__)
 
-dev = tf.config.list_physical_devices()
-print('Physical Devices  : ', dev)
-dev = tf.config.list_logical_devices()
-print('Available Devices : ', dev)
+#dev = tf.config.list_physical_devices()
+#print('Physical Devices  : ', dev)
+#dev = tf.config.list_logical_devices()
+#print('Available Devices : ', dev)
